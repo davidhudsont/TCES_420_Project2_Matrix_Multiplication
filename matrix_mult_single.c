377cf439b94237e2c697238b7f7394599e966fe4
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-10-24 10:16:55 -0700

    Update matrix_mult_single.c

--- matrix_mult_single.c
+++ matrix_mult_single.c
@@ -6,20 +6,14 @@ int main() {
 	srand(time(NULL));
 	int SIZE = 2000;
 	
-	int **A = (int **)malloc(SIZE * sizeof(int *));
-	int **B = (int **)malloc(SIZE * sizeof(int *));
-	int **C = (int **)malloc(SIZE * sizeof(int *));
-	
-	for (int i=0; i<SIZE; i++) {
-		A[i] = (int *)malloc(SIZE * sizeof(int));
-		B[i] = (int *)malloc(SIZE * sizeof(int));
-		C[i] = (int *)malloc(SIZE * sizeof(int));
-		
-	}
+	int *A = malloc(SIZE*SIZE * sizeof(int));
+	int *B = malloc(SIZE*SIZE * sizeof(int));
+	int *C = malloc(SIZE*SIZE * sizeof(int));
+
 	for (int rows=0; rows<SIZE; rows++) {
 		for (int columns=0; columns<SIZE; columns++) {
-			A[rows][columns] = rand()%10;
-			B[rows][columns] = rand()%10;
+			A[SIZE*rows+columns] = rand()%10;
+			B[SIZE*rows+columns] = rand()%10;
 		}
 	}
 	struct timespec begin, end; // create timing 
@@ -30,15 +24,18 @@ int main() {
 		for (int colb=0; colb<SIZE; colb++) {
 			int entry = 0;
 			for (int columns=0; columns<SIZE; columns++) {
-				entry = A[rows][columns]*B[columns][colb]+entry; 
+				entry += A[SIZE*rows+columns]*B[SIZE*columns+colb]; 
 			}
-			C[rows][colb] = entry;
+			C[SIZE*rows+colb] = entry;
 		}
 	}
 	clock_gettime(CLOCK_MONOTONIC,&end);
 	elapsed = end.tv_sec - begin.tv_sec;
 	elapsed += (end.tv_nsec - begin.tv_nsec)/1000000000.0;
 	printf("Operation took: %f\n\n", elapsed);
+	free(A);
+	free(B);
+	free(C);
 	return 0;
 	
 }
